5e844a641c394bfd5f0e3a385b1b8b26f5e4fcf5
Luis Gustavo <luisgustavo.nunes@icloud.com>
2026-03-04 18:01:05 -0300

    feat: add initial catalogue

--- app/_components/ui/infoCards.tsx
+++ app/_components/ui/infoCards.tsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
 import { Button } from "./button";
+import { Dialog, DialogContent, DialogTrigger } from "./dialog";
+import { Card, CardContent, CardFooter, CardHeader } from "./card";
+import { title } from "process";
 const InfoCards = () => {
   const data = [
     {
@@ -14,28 +18,101 @@ const InfoCards = () => {
       icon: "/instalogo3.png",
       color: "bg-gradient-to-r from-pink-500 to-yellow-500",
     },
+  ];
 
+  const price = "R$20,00";
+  const ervasData = [
     {
-      title: "Catálogo",
-      href: "#",
-      icon: "/catalogo.png",
-      color: "bg-yellow-500",
+      title: "Pura folha",
+      imgUrl: "purafolha.jpg",
+    },
+    {
+      title: "Energético",
+      imgUrl: "energetico.jpg",
+    },
+    {
+      title: "Burrito",
+      imgUrl: "burrito.jpg",
+    },
+    {
+      title: "Menta boldo",
+      imgUrl: "mentaboldo.jpg",
+    },
+    {
+      title: "Menta",
+      imgUrl: "menta.jpg",
+    },
+    {
+      title: "Menta limão",
+      imgUrl: "mentalimao.jpg",
+    },
+    {
+      title: "Menta Xtreme",
+      imgUrl: "mentaxtreme.jpg",
+    },
+    {
+      title: "Hortelã ice",
+      imgUrl: "hortelaice.jpg",
+    },
+    {
+      title: "Limão",
+      imgUrl: "limao.jpg",
     },
   ];
   return (
     <>
       {data.map((item, index) => (
         <div className="flex flex-col justify-start p-2" key={index}>
-          <Button key={index} className={`${item.color} justify-start h-20 hover:opacity-75 hover:${item.color}`} >
+          <Button
+            key={index}
+            className={`${item.color} justify-start h-20 hover:opacity-75 hover:${item.color}`}
+          >
             <img src={item.icon} alt={item.title} className="w-8 h-8" />
             {item.title && (
-              <a href={item.href} className="text-lg text-white">
+              <a
+                href={item.href}
+                className="text-lg text-white font-bebas-neue"
+              >
                 {item.title}
               </a>
             )}
           </Button>
         </div>
       ))}
+
+      <div className="flex flex-col justify-start p-2">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              className={`bg-yellow-400 font-bebas-neue text-lg justify-start h-20 hover:bg-yellow-400 hover:opacity-75`}
+            >
+              <img src="/catalogo.png" alt="catalogo" className="w-8 h-8" />
+              Catálogo
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent className="grid grid-cols-2 max-h-[80vh] overflow-y-auto">
+            {ervasData.map((item, index) => (
+              <Card key={index}>
+
+                <CardHeader className="flex border-b rounded-lg justify-center h-3"><h1 className="text-lg justify-center font-bebas-neue">{item.title}</h1></CardHeader>
+
+                <CardContent className="p-5">
+
+                  <img
+                    src={item.imgUrl}
+                    alt={item.title}
+                    className="h-full w-full"
+                  ></img>
+                </CardContent>
+
+                <CardFooter><h2 className="text-xl font-bebas-neue">{price}</h2></CardFooter>
+
+              </Card>
+            ))}
+          </DialogContent>
+        </Dialog>
+      </div>
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,34 @@
-
 import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { Button } from "./_components/ui/button";
+import { DialogTrigger } from "./_components/ui/dialog";
 import InfoCards from "./_components/ui/infoCards";
+import { Dialog } from "./_components/ui/dialog";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl lg:max-w-full lg:bg-[url('/folhas.jpg')] lg:items-center flex-col items-center pt-5 bg-white dark:bg-black sm:items-start">
-
         <div className="flex flex-col items-center gap-4 sm:items-start lg:items-center">
           <Avatar className="h-42 w-42">
             <AvatarImage src="logo.jpg" alt="Avatar" />
           </Avatar>
-          <h1 className="font-bebas-neue text-3xl">Bem-vindo ao Império das Ervas</h1>
-          <p className="font-bebas-neue text-xl">⎯ Fale conosco para mais informações. ⎯</p>
+          <h1 className="font-bebas-neue text-3xl">
+            Bem-vindo ao Império das Ervas
+          </h1>
+          <p className="font-bebas-neue text-xl">
+            ⎯ Fale conosco para mais informações. ⎯
+          </p>
         </div>
 
-        <div>
+        <div className="flex flex-col max-w-[350px] justify-start">
           <InfoCards />
         </div>
 
         <footer className="mt-auto w-full bg-zinc-200 py-5 text-center text-sm text-gray-500">
-          <p className="text-black">&copy; 2026 Império das Ervas. Todos os direitos reservados.</p>
+          <p className="text-black">
+            &copy; 2026 Império das Ervas. Todos os direitos reservados.
+          </p>
         </footer>
-
       </main>
     </div>
   );
